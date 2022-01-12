aaa4b37de6df729bc3ca22f972d3b49a40935437
“Daysha <+>
2022-01-12 11:00:32 -0800

    commit tree 965f89edc25ce1fe14752d7e1d0f7a1c66c16e45

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(array, string) {
+    let cards = []
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+    return cards;
+}
+
+function countDown(integer) {
+   while (integer >= 0)[
+       console.log(integer--)
+   ]
+}
+
+
